0f722c262c8aa09afcfae6a7ef1db7b2d3efc779
Loup <Superloup10@gmail.com>
2018-02-21 11:37:02 +0100

    Change build dir for Babel

---
+++ js/admin.js
@@ -0,0 +1,72 @@
+/* global $, socket*/
+
+$(() => {
+    readTypeDataFromServer();
+    checkRadioButton();
+    checkInputTypeWarning();
+});
+
+checkRadioButton = () => {
+    let radioButton = $(":radio");
+    radioButton.prop("checked", false);
+    radioButton.click(function () {
+        let tId = $(this).attr("id").replace("radio", "");
+        let dd = tId.charAt(0).toLowerCase() + tId.substring(1);
+        $('[data-group="sanctionRadio"]').attr("hidden", true);
+        $(`#${dd}`).attr("hidden", false);
+    });
+};
+
+sendTypeWarning = (listInput) => {
+    const titleTypeJson = ["title", "level", "timeNumber", "selectUnitTime"];
+    const typeJson = {
+        title: "",
+        level: 0,
+        timeNumber: 0,
+        selectUnitTime: ""
+    };
+    console.log(listInput);
+    let i = 0;
+    for(const input of listInput) {
+        console.log(input.value);
+        typeJson[titleTypeJson[i++]] = input.value;
+    }
+
+    console.log(typeJson);
+    $("#add-type-warning").click(() => {
+        socket.emit("modules.typeWarning", typeJson, (err, result) => {
+            alert(result);
+        });
+    });
+};
+
+checkInputTypeWarning = () => {
+    $("#form-type-warning input").focusout(function () {
+        if ($("#form-type-warning input:invalid").length === 0) {
+            toggleButton(false);
+        } else {
+            toggleButton(true);
+        }
+    });
+};
+
+toggleButton = (state) => {
+    let button = $("button");
+    button.attr("disabled", state);
+    if (!state) {
+        let listInput = $("#form-type-warning :input").not("button");
+        sendTypeWarning(listInput);
+    }
+};
+
+readTypeDataFromServer = () => {
+    const typeJson = {
+        title: "",
+        level: 0,
+        timeNumber: 0,
+        selectUnitTime: ""
+    };
+    socket.emit("modules.readTypeWarning", typeJson, (err, result) => {
+        console.log(result);
+    });
+};
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,23 +1,96 @@
-define("/admin/plugins/mff-moderation", ['settings'], (settings) => {
-    console.log("Je suis lu");
-    const ACP = {};
-    ACP.init = () => {
-        settings.load("mff-moderation", $('.mff-moderation'));
-        $("#save").on("click", () => {
-            console.log("Je suis lu");
-            settings.save("mff-moderation", $('.mff-moderation'), () => {
-                app.alert({
-                    type: 'success',
-                    alert_id: 'mff-moderation-saved',
-                    title: 'Settings saved',
-                    message: 'Please reload your NodeBB to apply these settings',
-                    clickfn: () => {
-                        socket.emit('admin.reload');
-                    }
-                });
-            });
-        });
-    };
+"use strict";
 
-    return ACP;
+/* global $, socket*/
+
+$(function () {
+    readTypeDataFromServer();
+    checkRadioButton();
+    checkInputTypeWarning();
 });
+
+checkRadioButton = function checkRadioButton() {
+    var radioButton = $(":radio");
+    radioButton.prop("checked", false);
+    radioButton.click(function () {
+        var tId = $(this).attr("id").replace("radio", "");
+        var dd = tId.charAt(0).toLowerCase() + tId.substring(1);
+        $('[data-group="sanctionRadio"]').attr("hidden", true);
+        $("#" + dd).attr("hidden", false);
+    });
+};
+
+sendTypeWarning = function sendTypeWarning(listInput) {
+    var titleTypeJson = ["title", "level", "timeNumber", "selectUnitTime"];
+    var typeJson = {
+        title: "",
+        level: 0,
+        timeNumber: 0,
+        selectUnitTime: ""
+    };
+    console.log(listInput);
+    var i = 0;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = listInput[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var input = _step.value;
+
+            console.log(input.value);
+            typeJson[titleTypeJson[i++]] = input.value;
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+
+    console.log(typeJson);
+    $("#add-type-warning").click(function () {
+        socket.emit("modules.typeWarning", typeJson, function (err, result) {
+            alert(result);
+        });
+    });
+};
+
+checkInputTypeWarning = function checkInputTypeWarning() {
+    $("#form-type-warning input").focusout(function () {
+        if ($("#form-type-warning input:invalid").length === 0) {
+            toggleButton(false);
+        } else {
+            toggleButton(true);
+        }
+    });
+};
+
+toggleButton = function toggleButton(state) {
+    var button = $("button");
+    button.attr("disabled", state);
+    if (!state) {
+        var listInput = $("#form-type-warning :input").not("button");
+        sendTypeWarning(listInput);
+    }
+};
+
+readTypeDataFromServer = function readTypeDataFromServer() {
+    var typeJson = {
+        title: "",
+        level: 0,
+        timeNumber: 0,
+        selectUnitTime: ""
+    };
+    socket.emit("modules.readTypeWarning", typeJson, function (err, result) {
+        console.log(result);
+    });
+};
+//# sourceMappingURL=admin.js.map
